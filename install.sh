346eb9812aed58bd224b1449f5f18e9deb54ffc7
wang_wang_lao@163.com <wang_wang_lao@163.com>
2022-06-16 15:29:20 +0800

    install tools

--- install.sh
+++ install.sh
@@ -1,2 +1,27 @@
-#/bin/sh
+#!/bin/bash
 
+OS=`uname`
+
+CUR_DIR=$(cd `dirname $0`; pwd)
+
+cd ${CUR_DIR}/src/postgis
+sh create_postgis_lib.sh
+
+cd ${CUR_DIR}
+PG_SRC_PATH=$CUR_DIR/../postgresql
+PG_CONTRIB_PATH=${PG_SRC_PATH}/contrib
+PG_LIB_PATH=`pg_config --libdir`
+
+if [ "${OS}" = "Linux" ];then 
+    sudo cp -f ${CUR_DIR}/src/postgis/libs/libpostgis.so ${PG_LIB_PATH}
+fi
+
+if [ "${OS}" = "Darwin" ];then
+    sudo cp -f ${CUR_DIR}/src/postgis/libs/libpostgis.a ${PG_LIB_PATH}
+fi
+
+rm -rf ${PG_CONTRIB_PATH}/laowangsql 
+cp -rf ${CUR_DIR}/src ${PG_CONTRIB_PATH}/laowangsql
+cd ${PG_CONTRIB_PATH}/laowangsql
+make 
+sudo make install
