cad623f85dd75f7afb4f8f8242610902fc33c318
wang_wang_lao@163.com <wang_wang_lao@163.com>
2022-06-16 15:27:45 +0800

    Map canvas for SQL

---
+++ src/canvas.c
@@ -0,0 +1,88 @@
+#include "canvas.h"
+#include <string.h>
+#include <liblwgeom.h>
+
+
+
+MapCanvas* lw_canvas_create(){
+    MapCanvas* canvas = (MapCanvas*)malloc(sizeof(MapCanvas));
+    canvas->surface = NULL;
+    canvas->cairo = NULL;
+    return canvas;
+}
+
+
+void lw_canvas_destroy(MapCanvas *canvas){
+    lw_canvas_end(canvas);
+}
+
+
+
+void lw_canvas_begin(MapCanvas *canvas){
+    if (canvas->format == CANVAS_JPG || 
+        canvas->format == CANVAS_PNG){
+        canvas->surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, canvas->pointWidth, canvas->pointHeight);
+        canvas->cairo = cairo_create(canvas->surface);
+    }else if(canvas->format == CANVAS_PDF){
+
+    }
+}
+
+
+void lw_canvas_end(MapCanvas *canvas){
+    if(canvas->cairo != NULL){
+        cairo_destroy(canvas->cairo);
+        canvas->cairo = NULL;
+    }
+    
+    if (canvas->surface != NULL){
+        cairo_surface_destroy(canvas->surface);
+        canvas->surface = NULL;
+    }
+}
+
+
+void lw_canvas_set_extent(MapCanvas *canvas, double minx, double miny, double maxx, double maxy){
+    canvas->minx = minx;
+    canvas->miny = miny;
+    canvas->maxx = maxx;
+    canvas->maxy = maxy;
+    canvas->centerx = (canvas->minx + canvas->maxx) * 0.5;
+    canvas->centery = (canvas->miny + canvas->maxy) * 0.5;
+}
+
+
+void lw_canvas_set_points_per_millimeter(MapCanvas* canvas, double pointsPerMilliMeter){
+    canvas->pointsPerMilliMeter = pointsPerMilliMeter;
+}
+
+
+void lw_canvas_set_size(MapCanvas *canvas, double width, double height){
+    canvas->width = width;
+    canvas->height = height;
+}
+
+
+void lw_canvas_set_format(MapCanvas *canvas, const char* format){
+    if ( strcasecmp(format, "png") == 0 ){
+        canvas->format = CANVAS_PNG;
+    }else if(strcasecmp(format, "jpg") == 0) {
+        canvas->format = CANVAS_JPG;
+    }else if (strcasecmp(format, "pdf") == 0){
+        canvas->format = CANVAS_PDF;
+    }
+}
+
+void lw_canvas_recalculate_parameters(MapCanvas* canvas){
+    double xscale, yscale;
+    canvas->pointWidth = canvas->width * canvas->pointsPerMilliMeter;
+    canvas->pointHeight = canvas->height * canvas->pointsPerMilliMeter;
+    xscale = canvas->width / (canvas->maxx - canvas->minx);
+    yscale = canvas->height / (canvas->maxy - canvas->miny);
+    canvas->scale = xscale > yscale ? yscale : xscale;
+    canvas->minx = canvas->centerx - canvas->pointWidth * 0.5 / canvas->scale;
+    canvas->maxx = canvas->centerx + canvas->pointWidth * 0.5 / canvas->scale;
+    canvas->miny = canvas->centery - canvas->pointHeight * 0.5 / canvas->scale;
+    canvas->maxy = canvas->centery + canvas->pointHeight * 0.5 / canvas->scale;
+}
+
