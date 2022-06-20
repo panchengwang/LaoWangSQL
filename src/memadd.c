235433eb7e9c050e99ad212c46ba500b6a4caf89
wang_wang_lao@163.com <wang_wang_lao@163.com>
2022-06-20 10:19:53 +0800

    in/out functions of MemAddress

--- src/memadd.c
+++ src/memadd.c
@@ -1,1 +1,31 @@
 #include <memadd.h>
+#include <postgres.h>
+#include <fmgr.h>
+#include <stdlib.h>
+
+
+// PG_MODULE_MAGIC;
+
+PG_FUNCTION_INFO_V1(lw_memadd_in);
+Datum lw_memadd_in(PG_FUNCTION_ARGS)
+{
+    char *str = PG_GETARG_CSTRING(0);
+    MemAddress *addr;
+    char *p; 
+    addr = (MemAddress*)palloc(sizeof(MemAddress));
+    addr->address = strtoul(str,&p,16);
+    PG_RETURN_POINTER(addr);
+}
+
+
+
+PG_FUNCTION_INFO_V1(lw_memadd_out);
+Datum lw_memadd_out(PG_FUNCTION_ARGS)
+{
+    MemAddress   *addr = (MemAddress *) PG_GETARG_POINTER(0);
+    char       *result;
+    unsigned long *p = (unsigned long*) (addr->address);
+    result = psprintf("%p", p);
+    PG_RETURN_CSTRING(result);
+}
+
--- src/memadd.h
+++ src/memadd.h
@@ -1,10 +1,14 @@
 #ifndef __MEMADD_H
 #define __MEMADD_H
-
+#include <postgres.h>
+#include <fmgr.h>
 
 typedef struct{
     unsigned long address;
 }MemAddress;
 
+
+Datum lw_memadd_in(PG_FUNCTION_ARGS);
+Datum lw_memadd_out(PG_FUNCTION_ARGS);
 
 #endif
